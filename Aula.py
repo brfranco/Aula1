98dc8b99ffdd31849f99ddc78cf2413457438e10
brfranco <brfranco@gmail.com>
2024-11-12 21:18:43 -0300

    desafio caique

--- Aula.py
+++ Aula.py
@@ -1,2 +1,13 @@
-print ("Ola Mundo")
-print ("Ola Mundo")
+a = 145
+b = 234
+soma = a + b
+print("A soma de", a, "e", b, "é:", soma)
+print(soma)
+print('Desafio do Caique')
+num1 = 145
+num2 = 234
+print(num1+num2)
+nome = "caique"
+idade = 20
+print("Olá, eu sou " + nome + " e tenho " + str(idade) + " anos.")
+
